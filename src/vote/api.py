5a8fa1878b26aade52ef6ad2b5585e4c8c0facd6
Tyler Sheldon <tyler@Kiwi-Machine-II.hsd1.wa.comcast.net>
2025-10-14 11:45:08 -0700

    Reformat API file

--- src/vote/api.py
+++ src/vote/api.py
@@ -10,10 +10,12 @@ from ninja import Schema
 
 router = Router()
 
+
 class EventCreation(Schema):
     name: str
     choices: List[str]
     electoral_system: Event.ElectoralSystem
+
 
 class EventDetails(Schema):
     name: str
@@ -21,19 +23,25 @@ class EventDetails(Schema):
     electoral_system: str
     closed: datetime | None
 
+
 class TokenBody(Schema):
     token: str
 
 
 @router.post("/event/create")
 def create_event(request: HttpRequest, payload: EventCreation):
-    event = Event.objects.create(name=payload.name, choices=payload.choices, electoral_system=payload.electoral_system.value)
+    event = Event.objects.create(
+        name=payload.name,
+        choices=payload.choices,
+        electoral_system=payload.electoral_system.value,
+    )
     return {"id": event.id, "event_token": event.token}
 
 
 @router.get("/event/{event_id}", response=EventDetails)
 def read_event(request: HttpRequest, event_id: str):
     return get_object_or_404(Event, pk=event_id)
+
 
 @router.post("/event/{event_id}/close")
 def close_event(request: HttpRequest, event_id: str, payload: TokenBody):
@@ -44,6 +52,7 @@ def close_event(request: HttpRequest, event_id: str, payload: TokenBody):
 
     event.closed = datetime.now()
     event.save()
+
 
 @router.post("/event/{event_id}/open")
 def open_event(request: HttpRequest, event_id: str, payload: TokenBody):
@@ -58,17 +67,20 @@ def open_event(request: HttpRequest, event_id: str, payload: TokenBody):
 
 # Ballots
 
+
 @router.post("/event/{event_id}/create-ballot")
 def create_ballot(request: HttpRequest, event_id: str, voter_name: Query[str]):
     event = get_object_or_404(Event, pk=event_id)
     ballot = Ballot.objects.create(event=event, voter_name=voter_name)
-    return {'ballot_id': ballot.id, 'ballot_token': ballot.token }
+    return {"ballot_id": ballot.id, "ballot_token": ballot.token}
+
 
 class BallotSubmission(Schema):
     token: str
     vote: str | List[str]
 
-@router.post('/ballot/{ballot_id}/submit')
+
+@router.post("/ballot/{ballot_id}/submit")
 def submit_ballot(request: HttpRequest, ballot_id: int, payload: BallotSubmission):
     ballot = get_object_or_404(Ballot, pk=ballot_id)
     if payload.token != str(ballot.token):
