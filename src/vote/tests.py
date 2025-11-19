514e634306a515d549f9ee487d18c44a9a395783
Jesudas Joseph <jesudasmjoseph@gmail.com>
2025-11-19 14:48:24 -0800

    Fix tests.

--- src/vote/tests.py
+++ src/vote/tests.py
@@ -12,7 +12,7 @@ class EventTestCase(TestCase):
         self.event = Event.objects.create(
             name="Big Cookoff",
             choices=["Tom's Texas Chili", "Jim's Vegan Chili", "Ed's Fusion Chili"],
-            electoral_system=Event.ElectoralSystem.PLURALITY,
+            electoral_system="PL",
         )
 
     def test_create_event(self):
@@ -33,7 +33,7 @@ class EventTestCase(TestCase):
     def test_read_event(self):
         response = self.client.get(
             f"/event/{self.event.id}",
-            query_params={"host_token": str(self.event.host_token)},
+            query_params={"token": str(self.event.host_token)},
         )
         self.assertEqual(response.status_code, 200)
 
@@ -41,41 +41,10 @@ class EventTestCase(TestCase):
         event = Event.objects.create(
             name="Big Cookoff",
             choices=["Tom's Texas Chili", "Jim's Vegan Chili", "Ed's Fusion Chili"],
-            electoral_system=Event.ElectoralSystem.PLURALITY,
+            electoral_system="PL",
         )
         response = self.client.get(f"/event/{event.id}")
         self.assertEqual(response.status_code, 403)
-
-    def test_get_ballot_statuses(self):
-        Ballot.objects.create(
-            event=self.event, voter_name="Ned", submitted=datetime.now(tz=UTC)
-        )
-        Ballot.objects.create(event=self.event, voter_name="Ted")
-
-        response = self.client.get(
-            f"/event/{self.event.id}/ballot-statuses",
-            query_params={"host_token": str(self.event.host_token)},
-        )
-
-        self.assertEqual(response.data, {"pending": ["Ted"], "submitted": ["Ned"]})
-
-    def test_get_ballot_results(self):
-        votes = [
-            ("Geoff", "Vote data"),
-            ("Lance", ["Could", "be", "any", "format"]),
-            ("Paul", {"We": 10, "don't": 5, "care": 0}),
-        ]
-        vote_data = [v[1] for v in votes]
-
-        for name, data in votes:
-            Ballot.objects.create(event=self.event, voter_name=name, vote=data)
-
-        response = self.client.get(
-            f"/event/{self.event.id}/ballot-results",
-            query_params={"host_token": str(self.event.host_token)},
-        )
-
-        self.assertListEqual(vote_data, response.data)
 
 
 class BallotTestCase(TestCase):
@@ -84,7 +53,7 @@ class BallotTestCase(TestCase):
         self.event = Event.objects.create(
             name="Big Cookoff",
             choices=["Tom's Texas Chili", "Jim's Vegan Chili", "Ed's Fusion Chili"],
-            electoral_system=Event.ElectoralSystem.PLURALITY,
+            electoral_system="PL",
         )
         self.ballot = Ballot.objects.create(event=self.event, voter_name="Becky")
 
@@ -129,7 +98,6 @@ class BallotTestCase(TestCase):
             query_params={"token": str(self.ballot.token)},
             json={"vote": "Tom's Texas Chili"},
         )
-        print(resubmission.status_code, resubmission.data)
         self.assertEqual(resubmission.status_code, 403)
 
     def test_ballot_submission_with_bad_token(self):
