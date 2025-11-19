2886cc7b822c51d8e9e315ccf428535ac72de613
Jesudas Joseph <jesudasmjoseph@gmail.com>
2025-11-19 14:40:02 -0800

    Add tags to APIs

--- src/vote/api.py
+++ src/vote/api.py
@@ -29,7 +29,7 @@ class EventCreationResponse(EventDetails):
     host_token: uuid.UUID
 
 
-@router.post("/event/create", response={201: EventCreationResponse})
+@router.post("/event/create", response={201: EventCreationResponse}, tags=["event"])
 def create_event(request, payload: EventCreation):
     event = Event.objects.create(
         name=payload.name,
@@ -39,7 +39,7 @@ def create_event(request, payload: EventCreation):
     return 201, event
 
 
-@router.get("/event/{event_id}", response=EventDetails)
+@router.get("/event/{event_id}", response=EventDetails, tags=["event"])
 def read_event(request, event_id: int, token: str = None):
     event = get_object_or_404(Event, pk=event_id)
 
@@ -57,7 +57,7 @@ def read_event(request, event_id: int, token: str = None):
     return event
 
 
-@router.post("/event/{event_id}/close")
+@router.post("/event/{event_id}/close", tags=["event"])
 def close_event(request: HttpRequest, event_id: str, host_token: str):
     event = get_object_or_404(Event, pk=event_id)
 
@@ -68,7 +68,7 @@ def close_event(request: HttpRequest, event_id: str, host_token: str):
     event.save()
 
 
-@router.post("/event/{event_id}/open")
+@router.post("/event/{event_id}/open", tags=["event"])
 def open_event(request: HttpRequest, event_id: str, host_token: str):
     event = get_object_or_404(Event, pk=event_id)
 
@@ -88,7 +88,7 @@ class BallotSchema(ModelSchema):
         fields = ["id", "voter_name", "vote", "created", "submitted"]
 
 
-@router.get("/event/{event_id}/ballots", response=List[BallotSchema])
+@router.get("/event/{event_id}/ballots", response=List[BallotSchema], tags=["ballot"])
 def list_ballots(request, event_id: str, token: str):
     event = get_object_or_404(Event, pk=event_id)
 
@@ -100,7 +100,7 @@ def list_ballots(request, event_id: str, token: str):
     return event.ballot_set.all().order_by("created", "submitted")
 
 
-@router.post("/event/{event_id}/create-ballot")
+@router.post("/event/{event_id}/create-ballot", tags=["ballot"])
 def create_ballot(
     request: HttpRequest, event_id: str, voter_name: str, share_token: str
 ):
@@ -124,7 +124,7 @@ class BallotSubmission(Schema):
     vote: Any
 
 
-@router.post("/ballot/{ballot_id}/submit")
+@router.post("/ballot/{ballot_id}/submit", tags=["ballot"])
 def submit_ballot(
     request: HttpRequest, ballot_id: int, token: str, payload: BallotSubmission
 ):
@@ -140,7 +140,7 @@ def submit_ballot(
     ballot.save()
 
 
-@router.get("/ballot/from-token", response=BallotSchema)
+@router.get("/ballot/from-token", response=BallotSchema, tags=["ballot"])
 def get_ballot_form_token(request: HttpRequest, token: uuid.UUID):
     ballots = Ballot.objects.filter(token=token)
 
@@ -150,7 +150,7 @@ def get_ballot_form_token(request: HttpRequest, token: uuid.UUID):
         return ballots.first()
 
 
-@router.get("/ballot/{ballot_id}", response=BallotSchema)
+@router.get("/ballot/{ballot_id}", response=BallotSchema, tags=["ballot"])
 def get_ballot(request, ballot_id: int, token: uuid.UUID):
     ballot = get_object_or_404(Ballot, pk=ballot_id)
 
